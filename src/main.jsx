cf90b2598a24185194077d17047b245cf527ef4c
DESKTOP-MK9SR3J\dell <siddhesh.gangdhar@bicsinfotech.com>
2026-01-05 19:24:17 +0530

    Siddhesh_Screen_Design

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import AppContextProvider from './context/AppContext.jsx'
-
+import './global.css'
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
   <AppContextProvider>
